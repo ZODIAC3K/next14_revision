3ef0c52786bc8c327a2411fdd97e643e48396011
zodiac3k <harshdeepanshustrix@gmail.com>
2024-10-29 16:54:49 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Account } from "@/components/account";
+import { Bonus } from "@/components/bonus";
+
+export default function Home() {
+  return (
+    <>
+      <Account />
+      <Bonus />
+    </>
+  );
+}
